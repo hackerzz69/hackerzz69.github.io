76a57a2b69e96753e365f56d6ec4636e8e3ec17b
kkonaog <kkonaog@fanet.dev>
2025-08-23 08:02:56 -0500

    More cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import MapView from '@/views/MapView.vue'
 
